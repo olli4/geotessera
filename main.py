b008a67ba261dc6e9fa6380100befebf0f3904b8
Anil Madhavapeddy <anil@recoil.org>
2025-07-02 10:36:00 +0100

    skeleton pooch

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from pytessera!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ tessera/main.py
@@ -0,0 +1,24 @@
+import os
+import importlib.resources
+import pooch
+
+def main():
+  version="v1"
+  POOCH = pooch.create(
+    path=pooch.os_cache("tessera"),
+    base_url="https://dl-1.tessera.wiki/{version}/global_0.1_degree_representation/",
+    version=version,
+    registry=None,
+  )
+
+  with importlib.resources.open_text("tessera", "registry_2024.txt") as registry_file:
+    POOCH.load_registry(registry_file)
+
+  fname=POOCH.fetch(
+    "2024/grid_0.15_52.05/grid_0.15_52.05.npy",
+    progressbar=True
+  )
+  print(fname)
+
+if __name__ == "__main__":
+    main()
